55c3e2ef3de8b3795dff9621a26531c1cfcb60b6
PatThompson88 <patrick.thompson688@gmail.com>
2022-12-27 02:21:03 -0500

    Adds statemachinebehaviour to change main camera view

---
+++ Assets/Components/CameraController/CameraController.cs
@@ -0,0 +1,23 @@
+using System;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera vCamOverhead;
+    [SerializeField] CinemachineVirtualCamera vCamLiveFight;
+    void Start()
+    {
+        StateLiveFightCam.onStateEnter += HandleFightLive;
+    }
+    private void HandleFightLive(object sender, System.EventArgs e)
+    {
+        DisableAllCams();
+        vCamLiveFight.gameObject.SetActive(true);
+    }
+    private void DisableAllCams()
+    {
+        vCamOverhead.gameObject.SetActive(false);
+        vCamLiveFight.gameObject.SetActive(false);
+    }
+}
