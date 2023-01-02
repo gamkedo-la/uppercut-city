34d1b8b5df0d3812faa87e62789c3732db37a9aa
PatThompson88 <patrick.thompson688@gmail.com>
2023-01-02 15:02:11 -0500

    Adds funtion to handle rotation - no logic yet

--- Assets/Characters/Common/FighterBehaviors.cs
+++ Assets/Characters/Common/FighterBehaviors.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class FighterBehaviors : MonoBehaviour
 {
     private Vector3 movementVector;
+    public void SetMovementVector(Vector2 movementInput){
+        movementVector.x = transform.position.x + movementInput.x;
+        movementVector.y = transform.position.y;
+        movementVector.z = transform.position.z + movementInput.y;
+    }
     private void HandleMovement(){
         Debug.Log("moving character");
         transform.position = Vector3.MoveTowards(
@@ -11,15 +16,13 @@ public class FighterBehaviors : MonoBehaviour
             0.02f
         );
     }
-    public void SetMovementVector(Vector2 movementInput){
-        movementVector.x = transform.position.x + movementInput.x;
-        movementVector.y = transform.position.y;
-        movementVector.z = transform.position.z + movementInput.y;
+    private void HandleRotation(){
+        // rotate towards opponent
     }
     private void FixedUpdate() {
         if(movementVector.magnitude > 0.1f){
             HandleMovement();
         }
-        // Add handle rotation
+        HandleRotation();
     }
 }
