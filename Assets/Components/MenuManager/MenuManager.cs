00e3f5d39e7aca062fde4c6dfe9e34f2c369587e
PatThompson88 <patrick.thompson688@gmail.com>
2023-02-03 17:46:21 -0500

    Game flow menu - click start - fight starts

--- Assets/Components/MenuManager/MenuManager.cs
+++ Assets/Components/MenuManager/MenuManager.cs
@@ -2,8 +2,14 @@ using System;
 using UnityEngine;
 public class MenuManager : MonoBehaviour
 {
+    [Header("Menu Containers")]
+    [SerializeField] public GameObject mainMenu;
     public static EventHandler<EventArgs> StartGame;
+    public void CloseAllMenus(){
+        mainMenu.SetActive(false);
+    }
     public void Btn_StartGame(){
+        CloseAllMenus();
         StartGame?.Invoke(this, EventArgs.Empty);
     }
     void Start()
