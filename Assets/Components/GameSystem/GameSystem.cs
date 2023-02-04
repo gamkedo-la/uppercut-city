506cf264d9bd7d0a90e152dc2da499460483f2ee
PatThompson88 <patrick.thompson688@gmail.com>
2023-02-04 02:24:34 -0500

    Character animator has 3 layers: legs, arms, spine

--- Assets/Components/GameSystem/GameSystem.cs
+++ Assets/Components/GameSystem/GameSystem.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
-
 public class GameSystem : MonoBehaviour
 {
     public Animator masterStateMachine;
