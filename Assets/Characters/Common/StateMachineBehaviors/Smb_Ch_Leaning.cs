77e882e03958a7b2d1459f976f72198a32121b10
PatThompson88 <patrick.thompson688@gmail.com>
2023-02-25 14:29:53 -0500

    Updates leaning mechanics(sm behavior, animator, avatar masks)

--- Assets/Characters/Common/StateMachineBehaviors/Smb_Ch_Leaning.cs
+++ Assets/Characters/Common/StateMachineBehaviors/Smb_Ch_Leaning.cs
@@ -1,23 +1,27 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-public class SMB_CH_Followthrough : StateMachineBehaviour
+
+public class Smb_Ch_Leaning : StateMachineBehaviour
 {
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+       animator.SetLayerWeight(1,1);
+    }
 
-    }
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-              
-    }
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       animator.ResetTrigger("PunchFollowThrough");
-       animator.SetBool("JabWindup", false);
-       animator.SetBool("CrossWindup", false);
+       animator.SetLayerWeight(1,0);
     }
+
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
