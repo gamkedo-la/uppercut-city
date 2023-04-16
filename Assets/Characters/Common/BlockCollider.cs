22a89cde663cb8358f8cadfde9cac0fe90d71114
PatThompson88 <patrick.thompson688@gmail.com>
2023-04-16 13:04:31 -0400

    Fixes conflict with credits menu, adds snowball-splat SFX x5

--- Assets/Characters/Common/BlockCollider.cs
+++ Assets/Characters/Common/BlockCollider.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class BlockCollider : MonoBehaviour
 {
     public delegate void BlockedPunch();
+    public SO_SfxGroup sfxBlockLight;
+    public SO_VfxGroup vfxBlockLight;
+    private AudioSource audioSource;
+    private ContactPoint collisionPoint;
     public event BlockedPunch onBlockedPunch;
     private void OnCollisionEnter(Collision collision)
     {
+        if(collision.gameObject.transform.root == transform.root) {return;}
         Debug.Log($"Punch Blocked {collision.gameObject.name}");
+
+        collisionPoint = collision.GetContact(0);
+        if (vfxBlockLight) Instantiate(vfxBlockLight,collisionPoint.point, Quaternion.LookRotation(collisionPoint.normal));
+        if (audioSource && sfxBlockLight) audioSource.PlayOneShot(sfxBlockLight.GetRandomSfx());
         onBlockedPunch?.Invoke();
         // trigger opponent's 'PunchBlocked' handler
         // play the blocked VFX
